0ce85024e74ef8c44c1fb2dfcaf1e62e411f7b43
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2025-03-28 20:46:27 -0300

    Update universidade.sql

--- universidade.sql
+++ universidade.sql
@@ -1,173 +1,121 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS RestauranteBD DEFAULT CHARACTER SET utf8;
+USE RestauranteBD;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+-- Tabela Cliente
+CREATE TABLE IF NOT EXISTS Cliente (
+  id_cliente INT AUTO_INCREMENT NOT NULL,
+  nome_cliente VARCHAR(100)  NULL,
+  endereco VARCHAR(200) NULL,
+  CPF CHAR(20) UNIQUE,
+  PRIMARY KEY (id_cliente)
+) ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `mydb`.`Professor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Professor` (
-  `Matricula` INT NOT NULL,
-  `Email` VARCHAR(256) NOT NULL,
-  `Nome` VARCHAR(200) NOT NULL,
-  `Coordenador_Matricula` INT NULL,
-  PRIMARY KEY (`Matricula`),
-  INDEX `fk_Professor_Professor1_idx` (`Coordenador_Matricula` ASC) VISIBLE,
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE,
-  CONSTRAINT `fk_Professor_Professor1`
-    FOREIGN KEY (`Coordenador_Matricula`)
-    REFERENCES `mydb`.`Professor` (`Matricula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- Tabela Mesa
+CREATE TABLE IF NOT EXISTS Mesa (
+  numero_mesa TINYINT(2) ZEROFILL NOT NULL,
+  PRIMARY KEY (numero_mesa)
+) ENGINE = InnoDB;
 
+-- Tabela Atendente
+CREATE TABLE IF NOT EXISTS Atendente (
+  matricula SMALLINT(3) zerofill NOT NULL,
+  nome_atendente VARCHAR(100) NOT NULL,
+  salario DECIMAL(10,2) NOT NULL,
+  AtendenteSenior_matricula SMALLINT(3) zerofill NULL,
+  PRIMARY KEY (matricula),
+  FOREIGN KEY (AtendenteSenior_matricula)
+    REFERENCES Atendente (matricula)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `mydb`.`Curso`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Curso` (
-  `Codigo` INT NOT NULL,
-  `Nome` VARCHAR(100) NOT NULL,
-  `CargaHoraria` INT NOT NULL,
-  `Professor_Matricula` INT NOT NULL,
-  PRIMARY KEY (`Codigo`),
-  INDEX `fk_Curso_Professor1_idx` (`Professor_Matricula` ASC) VISIBLE,
-  CONSTRAINT `fk_Curso_Professor1`
-    FOREIGN KEY (`Professor_Matricula`)
-    REFERENCES `mydb`.`Professor` (`Matricula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- Tabela Pedido
+CREATE TABLE IF NOT EXISTS Pedido (
+  numero_Pedido INT AUTO_INCREMENT NOT NULL,
+  hora_inicio DATETIME NOT NULL,
+  hora_fim DATETIME NULL,
+  duracao_segundos INT NULL,
+  Cliente_id INT NOT NULL,
+  Mesa_numero TINYINT(2) ZEROFILL NOT NULL,
+  Atendente_matricula SMALLINT(3) zerofill NOT NULL,
+  PRIMARY KEY (numero_Pedido),
+  FOREIGN KEY (Cliente_id) REFERENCES Cliente (id_cliente) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Mesa_numero) REFERENCES Mesa (numero_mesa) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Atendente_matricula) REFERENCES Atendente (matricula) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
+-- Tabela Prato
+CREATE TABLE IF NOT EXISTS Prato (
+  codigo_prato INT AUTO_INCREMENT NOT NULL,
+  nome_prato VARCHAR(100) NOT NULL,
+  preco_prato DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (codigo_prato)
+) ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `mydb`.`Materia`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Materia` (
-  `Codigo` INT NOT NULL,
-  `Nome` VARCHAR(100) NOT NULL,
-  `CargaHoraria` INT NOT NULL,
-  PRIMARY KEY (`Codigo`))
-ENGINE = InnoDB;
+-- Tabela Bebida
+CREATE TABLE IF NOT EXISTS Bebida (
+  codigo_bebida INT AUTO_INCREMENT NOT NULL,
+  nome_bebida VARCHAR(100) NOT NULL,
+  preco_bebida DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (codigo_bebida)
+) ENGINE = InnoDB;
 
+-- Tabela de relacionamento entre Prato e Pedido (N:N)
+CREATE TABLE IF NOT EXISTS Prato_has_Pedido (
+  Prato_codigo_prato INT NOT NULL,
+  Pedido_numero_Pedido INT NOT NULL,
+  PRIMARY KEY (Prato_codigo_prato, Pedido_numero_Pedido),
+  FOREIGN KEY (Prato_codigo_prato) REFERENCES Prato (codigo_prato) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Pedido_numero_Pedido) REFERENCES Pedido (numero_Pedido) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `mydb`.`Diciplina`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Diciplina` (
-  `Codigo` INT NOT NULL,
-  `Nome` VARCHAR(100) NOT NULL,
-  `Semestre` SMALLINT(2) NOT NULL,
-  `Vagas` INT(2) NOT NULL,
-  `Materia_Codigo` INT NOT NULL,
-  `Professor_Matricula` INT NOT NULL,
-  PRIMARY KEY (`Codigo`, `Materia_Codigo`, `Professor_Matricula`),
-  INDEX `fk_Diciplina_Materia1_idx` (`Materia_Codigo` ASC) VISIBLE,
-  INDEX `fk_Diciplina_Professor1_idx` (`Professor_Matricula` ASC) VISIBLE,
-  CONSTRAINT `fk_Diciplina_Materia1`
-    FOREIGN KEY (`Materia_Codigo`)
-    REFERENCES `mydb`.`Materia` (`Codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Diciplina_Professor1`
-    FOREIGN KEY (`Professor_Matricula`)
-    REFERENCES `mydb`.`Professor` (`Matricula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- Tabela de relacionamento entre Bebida e Pedido (N:N)
+CREATE TABLE IF NOT EXISTS Bebida_has_Pedido (
+  Bebida_codigo_bebida INT NOT NULL,
+  Pedido_numero_Pedido INT NOT NULL,
+  PRIMARY KEY (Bebida_codigo_bebida, Pedido_numero_Pedido),
+  FOREIGN KEY (Bebida_codigo_bebida) REFERENCES Bebida (codigo_bebida) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (Pedido_numero_Pedido) REFERENCES Pedido (numero_Pedido) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
+-- Tabela Telefone 
+CREATE TABLE IF NOT EXISTS Telefone (
+  numero_telefone VARCHAR(15) NOT NULL,
+  Atendente_matricula SMALLINT(3) zerofill NOT NULL,
+  PRIMARY KEY (numero_telefone, Atendente_matricula),
+  FOREIGN KEY (Atendente_matricula) REFERENCES Atendente (matricula) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `mydb`.`Aluno`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Aluno` (
-  `Matricula` INT NOT NULL,
-  `Nome` VARCHAR(200) NOT NULL,
-  `Telefone` VARCHAR(20) NOT NULL,
-  `Endereco` VARCHAR(250) NOT NULL,
-  `Curso_Codigo` INT NULL,
-  PRIMARY KEY (`Matricula`),
-  INDEX `fk_Aluno_Curso1_idx` (`Curso_Codigo` ASC) VISIBLE,
-  CONSTRAINT `fk_Aluno_Curso1`
-    FOREIGN KEY (`Curso_Codigo`)
-    REFERENCES `mydb`.`Curso` (`Codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- Triggers --
 
-
--- -----------------------------------------------------
--- Table `mydb`.`Materia_has_Curso`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Materia_has_Curso` (
-  `Materia_Codigo` INT NOT NULL,
-  `Curso_Codigo` INT NOT NULL,
-  PRIMARY KEY (`Materia_Codigo`, `Curso_Codigo`),
-  INDEX `fk_Materia_has_Curso_Curso1_idx` (`Curso_Codigo` ASC) VISIBLE,
-  INDEX `fk_Materia_has_Curso_Materia_idx` (`Materia_Codigo` ASC) VISIBLE,
-  CONSTRAINT `fk_Materia_has_Curso_Materia`
-    FOREIGN KEY (`Materia_Codigo`)
-    REFERENCES `mydb`.`Materia` (`Codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Materia_has_Curso_Curso1`
-    FOREIGN KEY (`Curso_Codigo`)
-    REFERENCES `mydb`.`Curso` (`Codigo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DELIMITER $$ 
+DELIMITER $$ 
+CREATE TRIGGER antes_pedido_insert
+BEFORE INSERT ON Pedido
+FOR EACH ROW
+BEGIN
+    IF NEW.hora_fim IS NOT NULL THEN
+        SET NEW.duracao_segundos = TIMESTAMPDIFF(SECOND, NEW.hora_inicio, NEW.hora_fim);
+    END IF;
+END $$
 
 
--- -----------------------------------------------------
--- Table `mydb`.`Diciplina_has_Aluno`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Diciplina_has_Aluno` (
-  `Diciplina_Codigo` INT NOT NULL,
-  `Diciplina_Materia_Codigo` INT NOT NULL,
-  `Diciplina_Professor_Matricula` INT NOT NULL,
-  `Aluno_Matricula` INT NOT NULL,
-  PRIMARY KEY (`Diciplina_Codigo`, `Diciplina_Materia_Codigo`, `Diciplina_Professor_Matricula`, `Aluno_Matricula`),
-  INDEX `fk_Diciplina_has_Aluno_Aluno1_idx` (`Aluno_Matricula` ASC) VISIBLE,
-  INDEX `fk_Diciplina_has_Aluno_Diciplina1_idx` (`Diciplina_Codigo` ASC, `Diciplina_Materia_Codigo` ASC, `Diciplina_Professor_Matricula` ASC) VISIBLE,
-  CONSTRAINT `fk_Diciplina_has_Aluno_Diciplina1`
-    FOREIGN KEY (`Diciplina_Codigo` , `Diciplina_Materia_Codigo` , `Diciplina_Professor_Matricula`)
-    REFERENCES `mydb`.`Diciplina` (`Codigo` , `Materia_Codigo` , `Professor_Matricula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Diciplina_has_Aluno_Aluno1`
-    FOREIGN KEY (`Aluno_Matricula`)
-    REFERENCES `mydb`.`Aluno` (`Matricula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+DELIMITER $$ 
+CREATE TRIGGER antes_pedido_update
+BEFORE UPDATE ON Pedido
+FOR EACH ROW
+BEGIN
+    -- Verifica se hora_fim foi alterada e está preenchida
+    IF NEW.hora_fim IS NOT NULL THEN
+        SET NEW.duracao_segundos = TIMESTAMPDIFF(SECOND, NEW.hora_inicio, NEW.hora_fim);
+    END IF;
+END $$
 
-
--- -----------------------------------------------------
--- Table `mydb`.`Email`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Email` (
-  `EnderecoEmail` VARCHAR(256) NOT NULL,
-  `Aluno_Matricula` INT NOT NULL,
-  PRIMARY KEY (`EnderecoEmail`),
-  INDEX `fk_Email_Aluno1_idx` (`Aluno_Matricula` ASC) VISIBLE,
-  CONSTRAINT `fk_Email_Aluno1`
-    FOREIGN KEY (`Aluno_Matricula`)
-    REFERENCES `mydb`.`Aluno` (`Matricula`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+DELIMITER ;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
