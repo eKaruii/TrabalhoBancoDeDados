061a2d5b232fa4766007c0c67c6765253dfe9dc8
MariaCBellote <159292487+MariaCBellote@users.noreply.github.com>
2025-03-29 16:52:35 -0300

    Update universidade.sql

--- universidade.sql
+++ universidade.sql
@@ -2,120 +2,110 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE SCHEMA IF NOT EXISTS RestauranteBD DEFAULT CHARACTER SET utf8;
-USE RestauranteBD;
+CREATE SCHEMA IF NOT EXISTS UniversidadeBD DEFAULT CHARACTER SET utf8;
+USE UniversidadeBD;
 
--- Tabela Cliente
-CREATE TABLE IF NOT EXISTS Cliente (
-  id_cliente INT AUTO_INCREMENT NOT NULL,
-  nome_cliente VARCHAR(100)  NULL,
-  endereco VARCHAR(200) NULL,
-  CPF CHAR(20) UNIQUE,
-  PRIMARY KEY (id_cliente)
-) ENGINE = InnoDB;
-
--- Tabela Mesa
-CREATE TABLE IF NOT EXISTS Mesa (
-  numero_mesa TINYINT(2) ZEROFILL NOT NULL,
-  PRIMARY KEY (numero_mesa)
-) ENGINE = InnoDB;
-
--- Tabela Atendente
-CREATE TABLE IF NOT EXISTS Atendente (
-  matricula SMALLINT(3) zerofill NOT NULL,
-  nome_atendente VARCHAR(100) NOT NULL,
-  salario DECIMAL(10,2) NOT NULL,
-  AtendenteSenior_matricula SMALLINT(3) zerofill NULL,
-  PRIMARY KEY (matricula),
-  FOREIGN KEY (AtendenteSenior_matricula)
-    REFERENCES Atendente (matricula)
-    ON DELETE SET NULL
+CREATE TABLE IF NOT EXISTS Professor (
+  Matricula INT(4) ZEROFILL NOT NULL,
+  Email VARCHAR(256) NOT NULL UNIQUE,
+  Nome VARCHAR(200) NOT NULL,
+  Coordenador_Matricula INT(4) ZEROFILL NULL,
+  PRIMARY KEY (Matricula),
+  FOREIGN KEY (Coordenador_Matricula)
+    REFERENCES Professor (Matricula)
+    ON DELETE SET NULL  -- Se o coordenador for excluído, a referência será removida.
     ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
--- Tabela Pedido
-CREATE TABLE IF NOT EXISTS Pedido (
-  numero_Pedido INT AUTO_INCREMENT NOT NULL,
-  hora_inicio DATETIME NOT NULL,
-  hora_fim DATETIME NULL,
-  duracao_segundos INT NULL,
-  Cliente_id INT NOT NULL,
-  Mesa_numero TINYINT(2) ZEROFILL NOT NULL,
-  Atendente_matricula SMALLINT(3) zerofill NOT NULL,
-  PRIMARY KEY (numero_Pedido),
-  FOREIGN KEY (Cliente_id) REFERENCES Cliente (id_cliente) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (Mesa_numero) REFERENCES Mesa (numero_mesa) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (Atendente_matricula) REFERENCES Atendente (matricula) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS Curso (
+  Codigo INT NOT NULL,
+  Nome VARCHAR(100) NOT NULL,
+  CargaHoraria INT NOT NULL DEFAULT 3600 CHECK (CargaHoraria = 3600),
+  Professor_Matricula INT(4) ZEROFILL NOT NULL,
+  PRIMARY KEY (Codigo),
+  FOREIGN KEY (Professor_Matricula)
+    REFERENCES Professor (Matricula)
+    ON DELETE cascade  -- Se o professor for excluído, o curso perderá a referência.
+    ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
--- Tabela Prato
-CREATE TABLE IF NOT EXISTS Prato (
-  codigo_prato INT AUTO_INCREMENT NOT NULL,
-  nome_prato VARCHAR(100) NOT NULL,
-  preco_prato DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (codigo_prato)
+CREATE TABLE IF NOT EXISTS Materia (
+  Codigo INT NOT NULL,
+  Nome VARCHAR(100) NOT NULL,
+  CargaHoraria INT NOT NULL CHECK(CargaHoraria >= 40),
+  PRIMARY KEY (Codigo)
 ) ENGINE = InnoDB;
 
--- Tabela Bebida
-CREATE TABLE IF NOT EXISTS Bebida (
-  codigo_bebida INT AUTO_INCREMENT NOT NULL,
-  nome_bebida VARCHAR(100) NOT NULL,
-  preco_bebida DECIMAL(10,2) NOT NULL,
-  PRIMARY KEY (codigo_bebida)
+CREATE TABLE IF NOT EXISTS Disciplina (
+  Codigo INT NOT NULL,
+  Nome VARCHAR(100) NOT NULL,
+  Semestre SMALLINT NOT NULL,
+  Vagas INT NOT NULL CHECK(Vagas <= 60),
+  Materia_Codigo INT NOT NULL,
+  Professor_Matricula INT(4) ZEROFILL NOT NULL,
+  PRIMARY KEY (Codigo, Materia_Codigo, Professor_Matricula),
+  FOREIGN KEY (Materia_Codigo)
+    REFERENCES Materia (Codigo)
+    ON DELETE CASCADE  -- Se a matéria for excluída, a disciplina perde a referência.
+    ON UPDATE CASCADE,
+  FOREIGN KEY (Professor_Matricula)
+    REFERENCES Professor (Matricula)
+    ON DELETE CASCADE  -- Se o professor for excluído, a disciplina perde a referência.
+    ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
--- Tabela de relacionamento entre Prato e Pedido (N:N)
-CREATE TABLE IF NOT EXISTS Prato_has_Pedido (
-  Prato_codigo_prato INT NOT NULL,
-  Pedido_numero_Pedido INT NOT NULL,
-  PRIMARY KEY (Prato_codigo_prato, Pedido_numero_Pedido),
-  FOREIGN KEY (Prato_codigo_prato) REFERENCES Prato (codigo_prato) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (Pedido_numero_Pedido) REFERENCES Pedido (numero_Pedido) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS Aluno (
+  Matricula INT NOT NULL,
+  Nome VARCHAR(200) NOT NULL,
+  Telefone VARCHAR(20) NOT NULL,
+  Endereco VARCHAR(250) NOT NULL,
+  Curso_Codigo INT NULL,
+  PRIMARY KEY (Matricula),
+  FOREIGN KEY (Curso_Codigo)
+    REFERENCES Curso (Codigo)
+    ON DELETE CASCADE -- Se o curso for excluído, o aluno perde a referência.
+    ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
--- Tabela de relacionamento entre Bebida e Pedido (N:N)
-CREATE TABLE IF NOT EXISTS Bebida_has_Pedido (
-  Bebida_codigo_bebida INT NOT NULL,
-  Pedido_numero_Pedido INT NOT NULL,
-  PRIMARY KEY (Bebida_codigo_bebida, Pedido_numero_Pedido),
-  FOREIGN KEY (Bebida_codigo_bebida) REFERENCES Bebida (codigo_bebida) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (Pedido_numero_Pedido) REFERENCES Pedido (numero_Pedido) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS Materia_has_Curso (
+  Materia_Codigo INT NOT NULL,
+  Curso_Codigo INT NOT NULL,
+  PRIMARY KEY (Materia_Codigo, Curso_Codigo),
+  FOREIGN KEY (Materia_Codigo)
+    REFERENCES Materia (Codigo)
+    ON DELETE CASCADE   -- Se a matéria for excluída, a relação com o curso também será removida.
+    ON UPDATE CASCADE,
+  FOREIGN KEY (Curso_Codigo)
+    REFERENCES Curso (Codigo)
+    ON DELETE CASCADE   -- Se o curso for excluído, a relação com a matéria também será removida.
+    ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
--- Tabela Telefone 
-CREATE TABLE IF NOT EXISTS Telefone (
-  numero_telefone VARCHAR(15) NOT NULL,
-  Atendente_matricula SMALLINT(3) zerofill NOT NULL,
-  PRIMARY KEY (numero_telefone, Atendente_matricula),
-  FOREIGN KEY (Atendente_matricula) REFERENCES Atendente (matricula) ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS Disciplina_has_Aluno (
+  Disciplina_Codigo INT NOT NULL,
+  Disciplina_Materia_Codigo INT NOT NULL,
+  Disciplina_Professor_Matricula INT(4) ZEROFILL NOT NULL,
+  Aluno_Matricula INT NOT NULL,
+  PRIMARY KEY (Disciplina_Codigo, Disciplina_Materia_Codigo, Disciplina_Professor_Matricula, Aluno_Matricula),
+  FOREIGN KEY (Disciplina_Codigo, Disciplina_Materia_Codigo, Disciplina_Professor_Matricula)
+    REFERENCES Disciplina (Codigo, Materia_Codigo, Professor_Matricula)
+    ON DELETE CASCADE   -- Se a disciplina for excluída, a associação com o aluno será removida.
+    ON UPDATE CASCADE,
+  FOREIGN KEY (Aluno_Matricula)
+    REFERENCES Aluno (Matricula)
+    ON DELETE CASCADE   -- Se o aluno for excluído, a associação com a disciplina será removida.
+    ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
--- Triggers --
-
-DELIMITER $$ 
-DELIMITER $$ 
-CREATE TRIGGER antes_pedido_insert
-BEFORE INSERT ON Pedido
-FOR EACH ROW
-BEGIN
-    IF NEW.hora_fim IS NOT NULL THEN
-        SET NEW.duracao_segundos = TIMESTAMPDIFF(SECOND, NEW.hora_inicio, NEW.hora_fim);
-    END IF;
-END $$
-
-
-DELIMITER $$ 
-CREATE TRIGGER antes_pedido_update
-BEFORE UPDATE ON Pedido
-FOR EACH ROW
-BEGIN
-    -- Verifica se hora_fim foi alterada e está preenchida
-    IF NEW.hora_fim IS NOT NULL THEN
-        SET NEW.duracao_segundos = TIMESTAMPDIFF(SECOND, NEW.hora_inicio, NEW.hora_fim);
-    END IF;
-END $$
-
-DELIMITER ;
+CREATE TABLE IF NOT EXISTS Email (
+  EnderecoEmail VARCHAR(256) NOT NULL,
+  Aluno_Matricula INT NOT NULL,
+  PRIMARY KEY (EnderecoEmail),
+  FOREIGN KEY (Aluno_Matricula)
+    REFERENCES Aluno (Matricula)
+    ON DELETE CASCADE   -- Se o aluno for excluído, o email associado será removido.
+    ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
